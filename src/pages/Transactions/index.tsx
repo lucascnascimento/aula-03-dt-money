9b4adb3ef5125a6a70ec1b9c9e389017161ce17a
Lucas Cordeiro <lucas.cordeiro@smarttbotter.com>
2023-02-28 18:54:31 -0300

    feat: create transactions table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,39 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./styles";
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width={"50%"}>Desenvolvimento de Site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>28/02/2022</td>
+            </tr>{" "}
+            <tr>
+              <td width={"50%"}>Desenvolvimento de Site</td>
+              <td>
+                <PriceHighlight variant="outcome">-R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>28/02/2022</td>
+            </tr>{" "}
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   );
 }
